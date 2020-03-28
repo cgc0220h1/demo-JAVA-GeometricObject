a19d45541f4c4258e3b6ca72d09cffc033dee8d3
vanduc2514 <vanduc2514@gmail.com>
2020-03-28 19:42:26 +0700

    Refactor

--- src/geometric/cylinder/Cylinder.java
+++ src/geometric/cylinder/Cylinder.java
@@ -95,13 +95,13 @@ public class Cylinder extends Circle {
 
     @Override
     public void inputData() {
+        super.inputData();
         System.out.print("Enter height: ");
         setHeight(super.scanner.nextDouble());
     }
 
     @Override
     public void displayData() {
-        System.out.println("Here is the result: ");
         System.out.printf("Surface Area: %.2f - Total Area: %.2f - Volume: %.2f \n \n"
                 , getSurfaceArea(), getTotalArea(), getVolume());
     }
