76c640f2e44019fc31d14ee70f70bb3c8d30e0a8
vanduc2514 <vanduc2514@gmail.com>
2020-03-28 16:03:47 +0700

    Bổ sung lớp triangle

--- src/Main.java
+++ src/Main.java
@@ -44,5 +44,15 @@ public class Main {
         cylinder = new Cylinder("black", true, 2.1,20);
         System.out.println(cylinder);
         System.out.println();
+
+        //Test class Triangle
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+        triangle = new Triangle(3,4,5);
+        System.out.println(triangle);
+        triangle = new Triangle("pink", false, 3,4,5);
+        System.out.println(triangle);
+        System.out.println();
+
     }
 }
---
+++ src/Triangle.java
@@ -0,0 +1,77 @@
+public class Triangle extends Geometric {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        side1 = 1;
+        side2 = 1;
+        side3 = 1;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public Triangle(String color, boolean filled, double side1, double side2, double side3) {
+        super(color, filled);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    @Override
+    public boolean getFilled() {
+        return super.getFilled();
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public double getArea() {
+        double halfPerimeter = getPerimeter() / 2;
+        return Math.sqrt(halfPerimeter
+                * (halfPerimeter - side1)
+                * (halfPerimeter - side2)
+                * (halfPerimeter - side3));
+    }
+
+    @Override
+    public String toString() {
+        return "A Triangle with side1 = "
+                + side1
+                + ", side2 = "
+                + side2
+                + " , side3 = "
+                + side3
+                + ", which is a subclass of "
+                + super.toString()
+                + "\n"
+                + "Perimeter = "
+                + super.decimalFormat.format(getPerimeter())
+                + " - Area = "
+                + super.decimalFormat.format(getArea());
+    }
+}
